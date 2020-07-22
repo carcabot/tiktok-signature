b841c719a1909692fedb93ab229116df7daf8a18
root <root@localhost>
2020-07-22 21:38:42 +0300

    fix trending

--- trending.py
+++ trending.py
@@ -12,20 +12,21 @@ import requests
 # PROFILE: 8,
 # ANALYTICS: 9
 
-signature = "oKxAeAAgEBgX6bvJMQKua6CsQWAAP4r"
-verifyFp = ""
+signature = "_02B4Z6wo00f01QlfOwwAAIBBUL8i4v.mws0JXj-AAB1R0c"
+verifyFp = "verify_kcxpglhh_ImVYPYCc_KZyz_4zwO_BnDt_TLuLcCCMD6Ob"
 
-referer = "https://www.tiktok.com/@ondymikula/video/6757762109670477061"
+referer = "https://www.tiktok.com/@ondymikula/video/6847563020290346245"
 
-url = "https://m.tiktok.com/api/item_list/?count=30&id=1&type=5&secUid=&maxCursor=1&minCursor=0&sourceType=12&appId=1233" + \
+url = "https://m.tiktok.com/share/item/list?secUid=&id=19274&type=3&count=30&minCursor=0&maxCursor=0&shareUid=&recType=&lang=" + \
     "&verifyFp=" + verifyFp + \
     "&_signature=" + signature
 
 request = requests.get(url, headers={"method": "GET",
-                                "accept-encoding": "gzip, deflate, br",
-                                "Referer": referer,
-                                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
-                                })
+                                     "accept-encoding": "gzip, deflate, br",
+                                     "cookie": "tt_webid_v2=1234567890",
+                                     "Referer": referer,
+                                     "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+                                     })
 
 data = request.text
 
