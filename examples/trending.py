7423a3f40630bef97bc54aca9b44728b6bb55372
Flo <root@localhost>
2021-06-09 15:43:45 +0300

    fix example

--- examples/trending.py
+++ examples/trending.py
@@ -19,9 +19,8 @@ if response.exitcode == 0:
                                                                      "cookie": "tt_webid_v2=1234567890;",
                                                                      "Referer": referer,
                                                                      "user-agent": signature['data']['navigator']['user_agent'],
-                                                                     "x-secsdk-csrf-token": "".join(
-                    random.choice(string.ascii_uppercase + string.ascii_lowercase)
-                    for i in range(92))
+                                                                     "csrf_session_id": "61d5ecf4e85e43e9a0b5ea9d9c759e7d",
+                                                                     "x-secsdk-csrf-token": "0001000000013de268bba45ec31dabbe5a6336e53647648d1e811e61cd86f127714dfa611a551686ea5a3b660761"
                                                                      })
 
     data = request.text
