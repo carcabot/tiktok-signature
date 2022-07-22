5c721a6fedc0a1711877e35b2d3bd17c2bfbadcb
carcabot <florin@carcabot.ro>
2022-07-23 02:34:59 +0300

    Update examples

--- examples/user-music.js
+++ examples/user-music.js
@@ -1,33 +1,33 @@
-const Signer = require('../');
-const axios = require('axios'); // NOTE: not adding this to package.json, you'll need to install it manually
+const Signer = require("..");
+const axios = require("axios"); // NOTE: not adding this to package.json, you'll need to install it manually
 
 // Get your SEC_UID from https://t.tiktok.com/api/user/detail/?aid=1988&uniqueId=username&language=it
 // where `username` is your TikTok username.
-const SEC_UID =
-  'MS4wLjABAAAAQ09e6Ck9CQrQQYAPLehEKMlvVS8XzmGcbNHTGXsXIZSIj7Pe21eYtDq0nzKy6-5V';
+const musicID = "7034143722082192134";
 
 // We use Apple, based on the issue comments in the repo, this helps prevent TikTok's captcha from triggering
 const USER_AGENT =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (Windows NT 10.0; Win64; x64) Chrome/90.0.4430.85 Safari/537.36';
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53";
 
 // This the final URL you make a request to for the API call, it is ALWAYS this, do not mistaken it for the signed URL
 const TT_REQ_PERM_URL =
-  'https://www.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=0.79&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=MacIntel&browser_version=5.0%20%28Macintosh%3B%20Intel%20Mac%20OS%20X%2010_15_7%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F103.0.0.0%20Safari%2F537.36&channel=tiktok_web&cookie_enabled=true&device_id=7123312453912577542&device_platform=web_pc&focus_state=true&from_page=user&history_len=2&is_fullscreen=false&is_page_visible=true&os=mac&priority_region=&referer=&region=RO&screen_height=1120&screen_width=1792&tz_name=Europe%2FBucharest&webcast_language=en&msToken=L0PQGEFijfQjvebd0lu-QX6MtiZzqYTcT5VUZLIQeGwWcvRroaWvCOqeG2knoRYXGK17Dwz4vzfyBcL6Yyg-1BNN0nYGrMvOSFSRUNEHDYamOQFGs--wOGxN6Bf1qFYZU8YC4-7df-MfqTlzeN8=&X-Bogus=DFSzKIVLsehANn/hSIeTcp7TlqSV&_signature=_02B4Z6wo00001zt3GhgAAIDAr6FBFuCZlDc7Zh6AAKwR1f';
+  "https://m.tiktok.com/api/music/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F101.0.4951.64%20Safari%2F537.36%20Edg%2F101.0.1210.53&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=false&from_page=music&history_len=1&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=&region=RO&screen_height=1080&screen_width=1920&tz_name=Europe%2FBucharest&verifyFp=verify_dca8729afe5c502257ed30b0b070dbdb&webcast_language=en&msToken=K8Xf-t_4RZ5n27zHsUPRyDIjpHQtfPeuHSvtbWzz0D0CQkX1UEyEdV0Xgx5BdbFPqKZ2McVCdlo1RM_u3o9FRglKoFa7TLZz2Yhd_fYRgWKhQDAq1TxQwLSTCz7Jp-EzVhopdNFO&X-Bogus=DFSzswVOLbUANCTQSwQvy2XyYJAm&_signature=_02B4Z6wo00001S9DBBwAAIDADOIqsG3-iK0vQwCAAClJd0";
 
 const PARAMS = {
   aid: 1988,
   count: 30,
   cursor: 0,
-  secUid: SEC_UID,
+  musicID: musicID,
+  secUid: "",
   cookie_enabled: true,
   screen_width: 0,
   screen_height: 0,
-  browser_language: '',
-  browser_platform: '',
-  browser_name: '',
-  browser_version: '',
-  browser_online: '',
-  timezone_name: 'Europe/London',
+  browser_language: "",
+  browser_platform: "",
+  browser_name: "",
+  browser_version: "",
+  browser_online: "",
+  timezone_name: "Europe/London",
 };
 
 async function main() {
@@ -37,7 +37,7 @@ async function main() {
   const qsObject = new URLSearchParams(PARAMS);
   const qs = qsObject.toString();
 
-  const unsignedUrl = `https://m.tiktok.com/api/post/item_list/?${qs}`;
+  const unsignedUrl = `https://m.tiktok.com/api/music/item_list/?${qs}`;
   const signature = await signer.sign(unsignedUrl);
   const navigator = await signer.navigator();
   await signer.close();
@@ -57,11 +57,9 @@ async function main() {
 
 async function testApiReq({ userAgent, xTtParams }) {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'user-agent': userAgent,
-      'x-tt-params': xTtParams,
-      'cookie': 's_v_web_id=verify_l5x14w5p_PoBTHsiu_tDkK_4mRM_BNjQ_OGYW3DkQAuqE'
+      "x-tt-params": xTtParams,
     },
     url: TT_REQ_PERM_URL,
   };
